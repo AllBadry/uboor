e8e3f2b948e2ef85b0a6a6cc1f2d0784088473b6
Abu Baker Al-Badry <adambakeralbdoor@gmail.com>
2026-07-07 15:19:43 +0300

    Remove sitemap plugin and add static sitemap.xml

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// استبدل 'repo-name' باسم المستودع الخاص بك على GitHub
 export default defineConfig({
   plugins: [react()],
   base: '/', 
