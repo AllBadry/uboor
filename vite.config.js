b785024c7eef39009e18a9a150c34582328f9ecd
Abu Baker Al-Badry <adambakeralbdoor@gmail.com>
2026-08-20 01:15:05 +0300

    fix indixing

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
-  server: {
-    host: '0.0.0.0', // هذا يسمح لـ Vite بالاستماع على جميع واجهات الشبكة
-    port: 5173       // تأكد أن المنفذ هو 5173
-  }
+  base: '/', // 🌟 هذا السطر يضمن تحميل الـ JS بشكل صحيح في كل الصفحات
 })
